fd6a902616eb8c179c130b2a0b4b4b6810862ee6
OlgaStarchenko <o9915530@gmail.com>
2026-06-02 18:37:40 +1000

    The validation form UI has been developed. At this stage, the form structure and its visual elements have been implemented: an input element for data entry and a button for form submission. Styles have been defined. The validation logic and form submission processing will be implemented using react-hook-form and yup.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,22 @@ import { yupResolver } from "./../node_modules/@hookform/resolvers/yup/src/yup";
 
 export function App() {
   return (
-    
-  )
+    <div className={styles.app}>
+      <form className={styles.form__app}>
+        <input name="email" type="email" className={styles.input__app} />
+        {/* <div className={styles.error__label}>Ошибка</div> */}
+        <input name="password" type="password" className={styles.input__app} />
+        {/* <div className={styles.error__label}>Ошибка</div> */}
+        <input
+          name="repeatPassword"
+          type="password"
+          className={styles.input__app}
+        />
+        {/* <div className={styles.error__label}>Ошибка</div> */}
+        <button className={styles.btn__send} type="submit" disabled={false}>
+          Отправить
+        </button>
+      </form>
+    </div>
+  );
 }
