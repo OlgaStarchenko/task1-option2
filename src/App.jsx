6fbb7236fee4edd63b5171688353a26cc89b1f68
OlgaStarchenko <o9915530@gmail.com>
2026-06-05 06:55:34 +1000

    The useForm hook is configured to manage form state. Form fields are registered using register. The onSubmit function receives form data and outputs it to the console.

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,35 @@ import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 
 export function App() {
+  const {
+    register,
+    handleSubmit,
+    formData: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className={styles.app}>
-      <form className={styles.form__app}>
-        <input name="email" type="email" className={styles.input__app} />
-        {/* <div className={styles.error__label}>Ошибка</div> */}
-        <input name="password" type="password" className={styles.input__app} />
+      <form className={styles.form__app} onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="email"
+          className={styles.input__app}
+          {...register("email")}
+        />
         {/* <div className={styles.error__label}>Ошибка</div> */}
         <input
-          name="repeatPassword"
           type="password"
           className={styles.input__app}
+          {...register("password")}
+        />
+        {/* <div className={styles.error__label}>Ошибка</div> */}
+        <input
+          type="password"
+          className={styles.input__app}
+          {...register("repeatPassword")}
         />
         {/* <div className={styles.error__label}>Ошибка</div> */}
         <button className={styles.btn__send} type="submit" disabled={false}>
